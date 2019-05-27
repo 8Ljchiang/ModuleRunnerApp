513eedb13e522d0de85136db13a363e1a22a777f
Jonathan C <jchiang@8thlight.com>
2019-05-26 19:52:41 -0500

    Create MockCommandHandlerResolver class and define protocol for CommandHandlerResolver and CommandHandler

---
+++ ModuleRunnerApp/ModuleRunnerApp/CommandHandler.swift
@@ -0,0 +1,13 @@
+//
+//  CommandHandler.swift
+//  ModuleRunnerApp
+//
+//  Created by Jonathan Chiang on 5/26/19.
+//  Copyright © 2019 chiangj. All rights reserved.
+//
+
+import Foundation
+
+protocol CommandHandlerProtocol {
+	
+}
