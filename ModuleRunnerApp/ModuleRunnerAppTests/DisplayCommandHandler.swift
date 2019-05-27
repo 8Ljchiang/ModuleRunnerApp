279f5e1b95cb441ae3ade21d5eeb8a355af40280
Jonathan C <jchiang@8thlight.com>
2019-05-27 07:53:21 -0500

    Add DisplayCommandHandler constructor test

--- ModuleRunnerApp/ModuleRunnerAppTests/DisplayCommandHandler.swift
+++ ModuleRunnerApp/ModuleRunnerAppTests/DisplayCommandHandler.swift
@@ -18,8 +18,9 @@ class DisplayCommandHandler: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testInitDisplayCommandHandler() {
+		let displayCH = DispalyCommandHandler();
+		
+		XCTAssertNotNil(displayCH);
     }
 }
