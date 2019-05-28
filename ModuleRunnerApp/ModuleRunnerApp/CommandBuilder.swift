f36653c6c776e55a61b100a59b645182b06efcbc
Jonathan C <jchiang@8thlight.com>
2019-05-28 15:13:12 -0500

    Add CommandBuilder promptForReadyCommand method

--- ModuleRunnerApp/ModuleRunnerApp/CommandBuilder.swift
+++ ModuleRunnerApp/ModuleRunnerApp/CommandBuilder.swift
@@ -37,4 +37,10 @@ class CommandBuilder {
 		let command = Command(type: CommandType.T3Rules, payload: payload);
 		return command;
 	}
+	
+	static func promptForReadyCommand() -> CommandProtocol {
+		let payload: [String: Any] = [:];
+		let command = Command(type: CommandType.T3PromptForReady, payload: payload);
+		return command;
+	}
 }
