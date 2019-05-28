b4e93ede01e3b5b5e915b27f78077c3127028ad1
Jonathan C <jchiang@8thlight.com>
2019-05-28 16:09:39 -0500

    Create BoardRenderer tests file

---
+++ ModuleRunnerApp/ModuleRunnerAppTests/BoardRendererTests.swift
@@ -0,0 +1,25 @@
+//
+//  BoardRendererTests.swift
+//  ModuleRunnerAppTests
+//
+//  Created by Jonathan Chiang on 5/28/19.
+//  Copyright © 2019 chiangj. All rights reserved.
+//
+
+import XCTest
+
+class BoardRendererTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
