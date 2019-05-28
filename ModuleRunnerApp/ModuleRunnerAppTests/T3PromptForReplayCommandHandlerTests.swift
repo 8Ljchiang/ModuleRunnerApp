602cf54060ed6c6d88ed9949a5258e18952809b8
Jonathan C <jchiang@8thlight.com>
2019-05-28 15:52:57 -0500

    Add T3PromptForReplayCommandHandler test for constructor

--- ModuleRunnerApp/ModuleRunnerAppTests/T3PromptForReplayCommandHandlerTests.swift
+++ ModuleRunnerApp/ModuleRunnerAppTests/T3PromptForReplayCommandHandlerTests.swift
@@ -18,8 +18,9 @@ class T3PromptForReplayCommandHandlerTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testInitT3PromptForReplayCommandHandler() {
+		let promptForReplayCH = T3PromptForReplayCommandHandler();
+		
+		XCTAssertNotNil(promptForReplayCH);
     }
 }
