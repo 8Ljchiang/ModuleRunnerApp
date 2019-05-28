775b9c1dc768161fecdc7416dde2e77b214c3f48
Jonathan C <jchiang@8thlight.com>
2019-05-28 10:21:58 -0500

    Add T3GameWelcomeCommandHandler test for execute method

--- ModuleRunnerApp/ModuleRunnerAppTests/T3GameWelcomeCommandHandlerTests.swift
+++ ModuleRunnerApp/ModuleRunnerAppTests/T3GameWelcomeCommandHandlerTests.swift
@@ -23,4 +23,20 @@ class T3GameWelcomeCommandHandlerTests: XCTestCase {
 		
 		XCTAssertNotNil(welcomeCH);
     }
+	
+	func testExecute() {
+		let mockGameModule = MockGameModule(defaultInputResponse: "Default Input Response");
+		let welcomeCH = T3GameWelcomeCommandHandler();
+		let payload: [String: Any] = [:];
+		let welcomeCommand = Command(type: CommandType.T3Welcome, payload: payload);
+		
+		let response = welcomeCH.execute(welcomeCommand, module: mockGameModule);
+		
+		XCTAssertNotNil(response);
+		XCTAssertEqual(2, response.commands.count);
+		XCTAssertEqual(CommandType.T3Display, response.commands[0].type);
+		XCTAssertEqual(T3Text.Welcome, response.commands[0].payload["text"]);
+		XCTAssertEqual(CommandType.T3Rules, response.commands[1].type);
+		XCTAssertEqual(0, response.errors.count);
+	}
 }
