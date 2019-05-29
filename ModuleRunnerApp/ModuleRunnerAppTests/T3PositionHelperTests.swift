980a23b65ea00692a0c46e40ae6b4388ea5095ef
Jonathan C <jchiang@8thlight.com>
2019-05-29 09:58:46 -0500

    Add T3PositionHelper test for getAvailablePositions

--- ModuleRunnerApp/ModuleRunnerAppTests/T3PositionHelperTests.swift
+++ ModuleRunnerApp/ModuleRunnerAppTests/T3PositionHelperTests.swift
@@ -74,4 +74,22 @@ class T3PositionHelperTests: XCTestCase {
 		
 		XCTAssertFalse(T3PositionHelper.isPositionAvailable(position: 10, moves: moves, boardSize: boardSize));
 	}
+	
+	func testGetAvailablePositions() {
+		let boardSize = 3;
+		let playerId1 = "P1";
+		let playerId2 = "P2";
+		let moves: [Move] = [
+			Move(playerId: playerId1, position: 1, marker: MarkerType.Marker1.rawValue),
+			Move(playerId: playerId2, position: 2, marker: MarkerType.Marker2.rawValue),
+			Move(playerId: playerId1, position: 3, marker: MarkerType.Marker1.rawValue),
+			Move(playerId: playerId2, position: 4, marker: MarkerType.Marker2.rawValue),
+			Move(playerId: playerId1, position: 5, marker: MarkerType.Marker1.rawValue),
+			Move(playerId: playerId2, position: 6, marker: MarkerType.Marker2.rawValue),
+		];
+		
+		let expectedPositions = [7, 8, 9];
+		
+		XCTAssertEqual(T3PositionHelper.getAvailablePositions(moves: moves, boardSize: boardSize));
+	}
 }
