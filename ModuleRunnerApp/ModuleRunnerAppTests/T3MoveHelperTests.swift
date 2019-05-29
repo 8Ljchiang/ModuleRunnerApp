7df0c0386c6a3344d083b5eec1d2d220f5fc30e5
Jonathan C <jchiang@8thlight.com>
2019-05-29 15:36:51 -0500

    Add T3MoveHelper test for generateMove method

--- ModuleRunnerApp/ModuleRunnerAppTests/T3MoveHelperTests.swift
+++ ModuleRunnerApp/ModuleRunnerAppTests/T3MoveHelperTests.swift
@@ -18,8 +18,25 @@ class T3MoveHelperTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testGenerateMove() {
+		let boardSize = 3;
+		let playerId1 = "P1";
+		let playerId2 = "P2";
+		let moves: [Move] = [
+			Move(playerId: playerId1, position: 1, marker: MarkerType.Marker1.rawValue),
+			Move(playerId: playerId2, position: 2, marker: MarkerType.Marker2.rawValue),
+			Move(playerId: playerId1, position: 3, marker: MarkerType.Marker1.rawValue),
+			Move(playerId: playerId2, position: 4, marker: MarkerType.Marker2.rawValue),
+			Move(playerId: playerId1, position: 5, marker: MarkerType.Marker1.rawValue),
+			Move(playerId: playerId2, position: 6, marker: MarkerType.Marker2.rawValue),
+			Move(playerId: playerId1, position: 7, marker: MarkerType.Marker1.rawValue),
+			Move(playerId: playerId2, position: 8, marker: MarkerType.Marker2.rawValue),
+		];
+		
+		let resultMove = T3MoveHelper.generateMove(playerId: playerId1, marker: MarkerType.Marker1.rawValue, moves: moves, boardSize: boardSize);
+		
+		XCTAssertEqual(playerId1, resultMove.playerId);
+		XCTAssertEqual(9, resultMove.position);
+		XCTAssertEqual(MarkerType.Marker1.rawValue, resultMove.marker);
     }
 }
