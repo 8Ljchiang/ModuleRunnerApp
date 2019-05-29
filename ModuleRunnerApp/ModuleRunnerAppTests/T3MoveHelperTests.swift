865b2fe798ba05a48ac1b870cc5a3b635d04c521
Jonathan C <jchiang@8thlight.com>
2019-05-29 15:29:15 -0500

    Add T3MoveHelper tests file

---
+++ ModuleRunnerApp/ModuleRunnerAppTests/T3MoveHelperTests.swift
@@ -0,0 +1,25 @@
+//
+//  T3MoveHelperTests.swift
+//  ModuleRunnerAppTests
+//
+//  Created by Jonathan Chiang on 5/29/19.
+//  Copyright © 2019 chiangj. All rights reserved.
+//
+
+import XCTest
+
+class T3MoveHelperTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
