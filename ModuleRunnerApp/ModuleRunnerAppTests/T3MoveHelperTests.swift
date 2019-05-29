8bfc09b946e2d8285c8fba5d8c56a2b4eab9f32b
Jonathan C <jchiang@8thlight.com>
2019-05-29 15:46:48 -0500

    Add T3MoveHelper test for generateMove method when no available positions

--- ModuleRunnerApp/ModuleRunnerAppTests/T3MoveHelperTests.swift
+++ ModuleRunnerApp/ModuleRunnerAppTests/T3MoveHelperTests.swift
@@ -40,4 +40,25 @@ class T3MoveHelperTests: XCTestCase {
 		XCTAssertEqual(9, resultMove?.position);
 		XCTAssertEqual(MarkerType.Marker1.rawValue, resultMove?.marker);
     }
+	
+	func testGenerateMoveWhenNoAvailablePositions() {
+		let boardSize = 3;
+		let playerId1 = "P1";
+		let playerId2 = "P2";
+		let moves: [Move] = [
+			Move(playerId: playerId1, position: 1, marker: MarkerType.Marker1.rawValue),
+			Move(playerId: playerId2, position: 2, marker: MarkerType.Marker2.rawValue),
+			Move(playerId: playerId1, position: 3, marker: MarkerType.Marker1.rawValue),
+			Move(playerId: playerId2, position: 4, marker: MarkerType.Marker2.rawValue),
+			Move(playerId: playerId1, position: 5, marker: MarkerType.Marker1.rawValue),
+			Move(playerId: playerId2, position: 6, marker: MarkerType.Marker2.rawValue),
+			Move(playerId: playerId1, position: 7, marker: MarkerType.Marker1.rawValue),
+			Move(playerId: playerId2, position: 8, marker: MarkerType.Marker2.rawValue),
+			Move(playerId: playerId2, position: 9, marker: MarkerType.Marker2.rawValue)
+		];
+		
+		let resultMove = T3MoveHelper.generateMove(playerId: playerId1, marker: MarkerType.Marker1.rawValue, moves: moves, boardSize: boardSize);
+		
+		XCTAssertNil(resultMove);
+	}
 }
