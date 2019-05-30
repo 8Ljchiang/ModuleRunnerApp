748edc91769e4dc476c08437f1b65452e7bf80c9
Jonathan C <jchiang@8thlight.com>
2019-05-30 11:14:13 -0500

    Add T3ReplayInstructionsCommandHandler test for execute method

--- ModuleRunnerApp/ModuleRunnerAppTests/T3ReplayInstructionsCommandHandlerTests.swift
+++ ModuleRunnerApp/ModuleRunnerAppTests/T3ReplayInstructionsCommandHandlerTests.swift
@@ -23,4 +23,21 @@ class T3ReplayInstructionsCommandHandlerTests: XCTestCase {
 		
 		XCTAssertNotNil(replayCH);
     }
+	
+	func testExecute() {
+		let replayCH = T3ReplayInstructionsCommandHandler();
+		let payload: [String: Any] = [:];
+		let replayCommand = Command(type: CommandType.T3ReplayInstructions, payload: payload);
+		let expectedCommandCount = 2;
+		let expectedErrorCount = 0;
+		
+		let response = replayCH.execute(replayCommand);
+		
+		XCTAssertNotNil(response);
+		XCTAssertEqual(expectedCommandCount, response.commands.count);
+		XCTAssertEqual(CommandType.T3Display, response.commands[0]);
+		XCTAssertEqual(T3Text.ReplayInstructions, response.commands[0].payload["text"]);
+		XCTAssertEqual(CommandType.T3PromptForReplay, response.commands[1]);
+		XCTAssertEqual(expectedErrorCount, response.errors.count);
+	}
 }
