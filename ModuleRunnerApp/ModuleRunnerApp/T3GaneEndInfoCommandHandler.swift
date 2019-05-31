9f16bf1f0b61a1bcf3ca85d9ae8b3dd81edf2b71
Jonathan C <jchiang@8thlight.com>
2019-05-30 19:23:48 -0500

    Create T3GameEndInfoCommandHandler class

---
+++ ModuleRunnerApp/ModuleRunnerApp/T3GaneEndInfoCommandHandler.swift
@@ -0,0 +1,17 @@
+//
+//  T3GaneEndInfoCommandHandler.swift
+//  ModuleRunnerApp
+//
+//  Created by Jonathan Chiang on 5/30/19.
+//  Copyright © 2019 chiangj. All rights reserved.
+//
+
+import Foundation
+
+class T3GameEndInfoCommandHandler: CommandHandlerProtocol {
+	init(readDataService: ReadDataServiceProtol) {}
+	
+	func execute(_ command: CommandProtocol, module: GameModuleProtocol) -> CommandHandlerResponseProtocol {
+		return CommandHandlerResponse();
+	}
+}
