8cf342f5e03b1667280b6ab0f3b59f685943ab1a
Jonathan C <jchiang@8thlight.com>
2019-06-03 16:35:58 -0500

    Add T3PromptForPositionCommandHandler test for execute method when no board size data exists

--- ModuleRunnerApp/ModuleRunnerAppTests/T3PromptForPositionCommandHandlerTests.swift
+++ ModuleRunnerApp/ModuleRunnerAppTests/T3PromptForPositionCommandHandlerTests.swift
@@ -183,4 +183,29 @@ class T3PromptForPositionCommandHandlerTests: XCTestCase {
 		XCTAssertEqual(expectedErrorCount, response.errors.count);
 		XCTAssertEqual("No moves data found.", response.errors[0]);
 	}
+	
+	func testExecuteWhenNoBoardSizeData() {
+		let inputPosition = "3";
+		let mockGameModule = MockGameModule(defaultInputResponse: inputPosition);
+		let payload: [String: Any] = [:];
+		let command = Command(type: CommandType.T3GameInfo, payload: payload);
+		var mockDataStore = DataStore();
+		mockDataStore.data = [
+			"moves": [],
+			"activePlayerIndex": 0,
+			"players": ["P1", "P2"],
+		];
+		
+		let mockReadDataService = MockReadDataService(dataStore: mockDataStore)
+		let promptForPositionCH = T3PromptForPositionCommandHandler(readDataService: mockReadDataService);
+		let expectedCommandCount = 0;
+		let expectedErrorCount = 1;
+		
+		let response = promptForPositionCH.execute(command, module: mockGameModule);
+		
+		XCTAssertNotNil(response);
+		XCTAssertEqual(expectedCommandCount, response.commands.count);
+		XCTAssertEqual(expectedErrorCount, response.errors.count);
+		XCTAssertEqual("No board size data found.", response.errors[0]);
+	}
 }
