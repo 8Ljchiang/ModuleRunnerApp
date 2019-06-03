29d5115f5a2b2623855186c316505c56d30b826a
Jonathan C <jchiang@8thlight.com>
2019-06-03 11:11:16 -0500

    Add T3GameAvailablePositionsCommandHandler test for execute method when all positions are available

--- ModuleRunnerApp/ModuleRunnerAppTests/T3GameAvailablePositionsCommandHandlerTests.swift
+++ ModuleRunnerApp/ModuleRunnerAppTests/T3GameAvailablePositionsCommandHandlerTests.swift
@@ -18,8 +18,33 @@ class T3GameAvailablePositionsCommandHandlerTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testInit() {
+		var dataStore: DataStore = DataStore();
+		dataStore.data = [
+			"playerCount": 2,
+			"boardSize": 3,
+			"aiSkill": 100,
+			"moves": [],
+			"players": ["P1", "P2"],
+			"activePlayerIndex": 0,
+			"winner": "P1",
+		];
+		let expectedPositionsString = "Available Positions: 1, 2, 3, 4, 5, 6, 7, 8, 9";
+		let mockReadDataService = MockReadDataService(dataStore: dataStore);
+		let mockGameModule = MockGameModule(defaultInputResponse: "Default input response");
+		let availablePositionsCH = T3GameAvailablePositionsCommandHandler(readDataService: mockReadDataService);
+		let payload: [String: Any] = [:];
+		let command = Command(type: CommandType.T3GameAvailablePositions, payload: payload);
+		let expectedCommandCount = 3;
+		let expectedErrorCount = 0;
+		
+		let response = availablePositionsCH.execute(command, module: mockGameModule);
+		
+		XCTAssertNotNil(response);
+		XCTAssertEqual(expectedCommandCount, response.commands.count);
+		XCTAssertEqual(expectedErrorCount, response.errors.count);
+		
+		XCTAssertEqual(CommandType.T3Display, response.commands[0].type);
+		XCTAssertEqual(expectedPositionsString, response.commands[0].payload["text"] as? String);
     }
 }
