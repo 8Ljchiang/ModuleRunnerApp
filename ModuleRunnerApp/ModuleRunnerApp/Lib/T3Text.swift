63dc29c7239ea1608037429f4fb3e1ae54499d51
Jonathan C <jchiang@8thlight.com>
2019-06-04 17:07:47 -0500

    Update T3Text for replay instructions

--- ModuleRunnerApp/ModuleRunnerApp/Lib/T3Text.swift
+++ ModuleRunnerApp/ModuleRunnerApp/Lib/T3Text.swift
@@ -13,6 +13,6 @@ struct T3Text {
 	static let welcome = "Welcome to Tic Tac Toe";
 	static let rules = "Rules:\n1. Choose a position on the board (1-9)\n2. Match three in a row to win.";
 	static let promptForReadyInstructions = "Type 'ready' to begin...";
-	static let replayInstructions = "Type 'replay' to play again..."
+	static let replayInstructions = "Type 'quit' to end or 'replay' to play again..."
 	static let openSpace = " ";
 }
